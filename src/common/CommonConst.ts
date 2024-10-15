cea09d8d959f4c9b3ed11007627f35b8c5d5ef3f
Zachary <zjobby52@gmail.com>
2024-10-15 18:59:00 +0800

    Added constant and data structure

--- src/common/CommonConst.ts
+++ src/common/CommonConst.ts
@@ -2,3 +2,4 @@ export const PASS_REGEX: RegExp = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\/])[A-Za-z\
 export const USERNAME_REGEX: RegExp = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9])*$/;
 export const CURRENT_USERNAME_KEY = "current_username";
 export const JWT_TOKEN_KEY = "token";
+export const IS_AUTHENTICATED_KEY = "is_authenticated";
