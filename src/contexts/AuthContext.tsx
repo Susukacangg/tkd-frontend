dd57673cedbf8f51b537838263b59e7f5c3b77cc
Zachary <zjobby52@gmail.com>
2024-10-16 01:10:30 +0800

    Implement refresh token function interceptor, fixed authentication bug to keep users logged in when refresh token has not yet expired. Added https.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,42 +1,57 @@
-import {createContext, ReactNode, useContext, useState} from "react";
+import {createContext, ReactNode, useContext, useEffect, useState} from "react";
 import {toast} from "sonner";
 import {TOAST_CUSTOM_CLOSE_BTN} from "../common/toast-custom-close-btn.tsx";
-import {CURRENT_USERNAME_KEY, JWT_TOKEN_KEY} from "../common/CommonConst.ts";
+import {IS_AUTHENTICATED_KEY, JWT_TOKEN_KEY} from "../common/CommonConst.ts";
 import LoginResponse from "../dto/LoginResponse.ts";
 import AuthContextProps from "../component-props/auth-context-props.ts";
 import IamService from "../service/iam-service.ts";
+import UserAccount from "../dto/UserAccount.ts";
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export function AuthProvider({ children }: {children: ReactNode}) {
-    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem(JWT_TOKEN_KEY) !== null);
-    const [currentUsername, setCurrentUsename] = useState<string | null>(localStorage.getItem(CURRENT_USERNAME_KEY));
+    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem(IS_AUTHENTICATED_KEY) !== null);
+    const [currentUser, setCurrentUser] = useState<UserAccount | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            if(isAuthenticated) {
+                try {
+                    console.log("load user details");
+                    const userDetails = await IamService.getUserDetails();
+                    setCurrentUser(userDetails);
+                } catch (error: any) {
+                    console.error(error);
+                }
+            }
+        })();
+    }, [isAuthenticated])
 
     const loginUser = (response: LoginResponse) => {
-        localStorage.setItem(CURRENT_USERNAME_KEY, response.username);
-        localStorage.setItem(JWT_TOKEN_KEY, response.token)
+        localStorage.setItem(IS_AUTHENTICATED_KEY, "true");
+        sessionStorage.setItem(JWT_TOKEN_KEY, response.token)
 
         setIsAuthenticated(true);
-        setCurrentUsename(response.username);
     };
 
     const logoutUser = async () => {
         try {
             const logoutMessage = await IamService.logout();
 
-            localStorage.removeItem(CURRENT_USERNAME_KEY);
-            localStorage.removeItem(JWT_TOKEN_KEY);
+            localStorage.removeItem(IS_AUTHENTICATED_KEY);
+            sessionStorage.removeItem(JWT_TOKEN_KEY);
             toast.success(logoutMessage, TOAST_CUSTOM_CLOSE_BTN);
 
             setIsAuthenticated(false);
-            setCurrentUsename(null);
+
+            window.location.href = "/login"
         } catch (error: any) {
             toast.error(error.message, TOAST_CUSTOM_CLOSE_BTN);
         }
     };
 
     return(
-        <AuthContext.Provider value={{isAuthenticated, currentUsername, loginUser, logoutUser}}>
+        <AuthContext.Provider value={{currentUser, isAuthenticated, loginUser, logoutUser}}>
             {children}
         </AuthContext.Provider>
     );
