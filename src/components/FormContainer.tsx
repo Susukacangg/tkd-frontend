fe633487b40d2043d03aaba8387e4e7a296c8279
Zachary <zjobby52@gmail.com>
2024-10-18 14:04:03 +0800

    Changed my

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -3,7 +3,7 @@ import FormContainerProps from "../component-props/form-container-props.ts";
 
 function FormContainer({headerString, subHeaderString, children, formFooter}: FormContainerProps) {
     return (
-        <div className={"container flex flex-col justify-center items-start w-full my-12 mx-auto"}>
+        <div className={"container flex flex-col justify-center items-start w-full mt-10 mb-14 mx-auto"}>
             <Typography variant={"h4"} fontWeight={"bold"}>
                 {headerString}
             </Typography>
