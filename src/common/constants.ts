271c56910b397706eca6a5984810caabc752e7fc
Zachary <zjobby52@gmail.com>
2024-10-20 23:07:45 +0800

    Renamed file and removed a couple unused constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,3 @@
 export const PASS_REGEX: RegExp = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\/])[A-Za-z\d@$!%*?&\/]{8,}$/;
 export const USERNAME_REGEX: RegExp = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9])*$/;
-export const CURRENT_USERNAME_KEY = "current_username";
-export const JWT_TOKEN_KEY = "token";
 export const IS_AUTHENTICATED_KEY = "is_authenticated";
