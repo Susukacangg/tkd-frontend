6a6164b7c0485c42bd2f12ae9feb77fb2b666ff5
Zachary <zjobby52@gmail.com>
2024-10-20 23:09:12 +0800

    Removed unused class

--- src/common/HttpStatus.ts
+++
@@ -1,6 +0,0 @@
-export class HttpStatus {
-    static SUCCESS: number = 200;
-    static CONFLICT: number = 409;
-    static BAD_CREDENTIALS: number = 403;
-    static SERVER_ERROR: number  = 500;
-}
