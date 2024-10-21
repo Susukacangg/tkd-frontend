530f50d2a9aad0512519f6c8e99a1277990406ac
Zachary <zjobby52@gmail.com>
2024-10-21 10:31:19 +0800

    Renaming

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,21 +4,21 @@ import {TOAST_CUSTOM_CLOSE_BTN} from "../common/toast-custom-close-btn.tsx";
 import {IS_AUTHENTICATED_KEY} from "../common/constants.ts";
 import AuthContextProps from "../component-props/auth-context-props.ts";
 import IamService from "../service/iam-service.ts";
-import UserAccount from "../dto/UserAccount.ts";
+import UserView from "../dto/UserView.ts";
 import iamService from "../service/iam-service.ts";
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export function AuthProvider({ children }: {children: ReactNode}) {
     const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem(IS_AUTHENTICATED_KEY) !== null);
-    const [currentUser, setCurrentUser] = useState<UserAccount | null>(null);
+    const [currentUser, setCurrentUser] = useState<UserView | null>(null);
 
     useEffect(() => {
         const controller = new AbortController();
         (async () => {
             if(isAuthenticated) {
                 try {
-                    const response: UserAccount = await iamService.getUserDetails(controller);
+                    const response: UserView = await iamService.getUserDetails(controller);
                     setCurrentUser(response);
                 } catch (error: any) {
                     // do nothing
--- src/dto/Word.ts
+++ src/dto/Word.ts
@@ -1,7 +1,7 @@
 import Translation from "./Translation.ts";
 import UsageExamples from "./UsageExamples.ts";
 
-export default interface DictionaryWord {
+export default interface Word {
     word: string;
     translations: Translation[];
     usageExamples: UsageExamples[];
