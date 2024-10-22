cfa14925f650263fdda6fa7a84b1e7cebc34f7e9
Zachary <zjobby52@gmail.com>
2024-10-23 02:08:55 +0800

    Integrate random word API call

--- src/service/dictionary-service.ts
+++ src/service/dictionary-service.ts
@@ -17,6 +17,21 @@ export default class DictionaryService {
         }
     }
 
+    static async getRandomWords(pageNumber: number, controller: AbortController): Promise<any> {
+        try {
+            const response = await dictionaryClient.get("/dict/random", {
+                params: {pageNumber: pageNumber},
+                timeout: 3000,
+                timeoutErrorMessage: "Failed to get random words",
+                signal: controller.signal,
+            })
+
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
     static async findWord(wordId: number, controller: AbortController): Promise<DictionaryItem> {
         try {
             const response = await dictionaryClient.get(`/dict/${wordId}`, {
