e7a893328f9d0ad9ee1784c748b36d1badb5ee04
Zachary <zjobby52@gmail.com>
2024-10-24 00:13:26 +0800

    paged API response

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,17 +10,19 @@ import DictionaryItem from "../dto/DictionaryItem.ts";
 
 function Home() {
     const [numPages, setNumPages] = useState<number>(1);
-    const [currentPage, setCurrentPage] = useState<number>(1);
     const [dictionaryItems, setDictionaryItems] = useState<DictionaryItem[]>([]);
+    const [pagedDictionaryItems, setPagedDictionaryItems] = useState<DictionaryItem[]>([]);
+    const numResultsDisplay: number = 10;
 
     useEffect(() => {
         const controller = new AbortController();
 
         (async() => {
             try {
-                const response = await DictionaryService.getRandomWords(currentPage, controller);
-                setNumPages(response.totalPages);
-                setDictionaryItems(response.content);
+                const response: DictionaryItem[] = await DictionaryService.getRandomWords(controller);
+                setNumPages(Math.ceil(response.length / numResultsDisplay));
+                setDictionaryItems(response);
+                setPagedDictionaryItems(response.slice(0, 10));
                 console.log(response);
             } catch (error: any) {
                 if(error.name !== 'CanceledError')
@@ -29,19 +31,21 @@ function Home() {
         })()
 
         return () => controller.abort();
-    }, [currentPage])
+    }, [])
 
     return (
     <>
         <Header enableSearchBar={false}/>
         <div className={"flex flex-col gap-6 justify-center mx-auto my-12 w-3/5"}>
             <HeroBanner/>
-            <WordList dictionaryItems={dictionaryItems} />
+            <WordList dictionaryItems={pagedDictionaryItems} />
             <div className="flex justify-center">
                 <Pagination count={numPages}
                             size={"large"}
                             color={"primary"}
-                            onChange={(_, page) => setCurrentPage(page)}/>
+                            sx={{}}
+                            onChange={(_, page) =>
+                                setPagedDictionaryItems(dictionaryItems.slice((page - 1) * numResultsDisplay, (page * numResultsDisplay)))}/>
             </div>
         </div>
     </>
