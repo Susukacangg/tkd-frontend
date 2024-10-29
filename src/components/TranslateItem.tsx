73d483820f2a9162e968119374af7fba4c86b07e
Zachary <zjobby52@gmail.com>
2024-10-29 19:19:56 +0800

    Minor UI changes

--- src/components/TranslateItem.tsx
+++ src/components/TranslateItem.tsx
@@ -3,10 +3,10 @@ import {GTranslate} from "@mui/icons-material";
 
 function TranslateItem({translation}: {translation: string}) {
     return (
-        <div className={"flex flex-row justify-between items-center"}>
+        <div className={"flex flex-row justify-between items-center mt-1"}>
             <div className={"w-fit flex flex-row items-center"}>
                 <GTranslate fontSize={"large"}
-                            className={"mr-4 bg-gray-200 p-3 rounded-md"}/>
+                            className={"mr-6 bg-gray-200 p-1 rounded-md"}/>
                 <Typography variant="h6" fontWeight={"bold"}>
                     {translation}
                 </Typography>
--- src/components/UsageExampleItem.tsx
+++ src/components/UsageExampleItem.tsx
@@ -2,11 +2,11 @@ import {Typography} from "@mui/material";
 import {UsageExampleProps} from "../component-props/usage-example-props.ts";
 import {FormatQuote} from "@mui/icons-material";
 
-function UsageExampleItem({kadazanSentence, englishSentence}: UsageExampleProps) {
+function UsageExampleItem({kadazanSentence, originalSentence}: UsageExampleProps) {
     return (
-        <div className={"flex flex-row mr-4 items-center"}>
+        <div className={"flex flex-row mr-4 items-center mt-1"}>
             <FormatQuote fontSize={"large"}
-                         className={"mr-4 bg-gray-200 p-3 rounded-md"}/>
+                         className={"mr-4 bg-gray-200 p-1 rounded-md"}/>
 
             {/*sentence example*/}
             <div className={"flex flex-col"}>
@@ -15,9 +15,9 @@ function UsageExampleItem({kadazanSentence, englishSentence}: UsageExampleProps)
                     {kadazanSentence}
                 </Typography>
 
-                {/*ENGLISH EXAMPLE*/}
+                {/*ORIGINAL EXAMPLE*/}
                 <Typography variant={"subtitle1"}>
-                    {englishSentence}
+                    {originalSentence}
                 </Typography>
             </div>
         </div>
