4e269b70cc91255c6787cef234b6c5e07e3b4df4
Zachary <zjobby52@gmail.com>
2024-11-01 04:21:10 +0800

    Added loading feedback, fixed bugs and use CenteredContainer
    (cherry picked from commit d5470d288e410dcb2e30b04022cd17fd3db01eea)

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -3,21 +3,26 @@ import {useEffect, useState} from "react";
 import WordList from "../components/WordList.tsx";
 import {useSearchParams} from "react-router-dom";
 import DictionaryService from "../service/dictionary-service.ts";
-import {Pagination, Typography} from "@mui/material";
+import {CircularProgress, Pagination, Typography} from "@mui/material";
 import Word from "../dto/Word.ts";
+import CenteredContainer from "../components/CenteredContainer.tsx";
 
 function SearchResult() {
     const [words, setWords] = useState<Word[]>([]);
-    const [numPages, setNumPages] = useState(1);
+    const [numPages, setNumPages] = useState(0);
+    const [pageNum, setPageNum] = useState(1);
     const [searchParams] = useSearchParams();
     const searchString = searchParams.get("searchString");
 
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
         const controller = new AbortController();
+        setIsLoading(true);
         (async () => {
             if (searchString !== undefined) {
                 try {
-                    const response: any = await DictionaryService.searchWord(searchString as string, 1, controller);
+                    const response: any = await DictionaryService.searchWord(searchString as string, pageNum, controller);
                     if(response !== "") {
                         setWords(response.content);
                         setNumPages(response.totalPages);
@@ -29,29 +34,38 @@ function SearchResult() {
                     if(error.name !== 'CanceledError')
                         console.error(error);
                 }
+                setIsLoading(false);
             }
         })();
 
-        return () => controller.abort();
-    }, [searchString])
+        return () => {
+            controller.abort();
+            setIsLoading(false);
+        }
+    }, [searchString, pageNum])
 
     return (
         <>
             <Header/>
-            <div className={"flex flex-col gap-6 justify-center mx-auto mt-20 mb-12 w-3/5"}>
-                {words.length > 0? (
-                    <>
-                        <WordList words={words}/>
-                        <div className="flex justify-center">
-                            <Pagination count={numPages}/>
-                        </div>
-                    </>
-                ): (
-                    <Typography variant={"h3"}>
-                        No results found for "{searchString}"
-                    </Typography>
-                )}
-            </div>
+            <CenteredContainer>
+                {!isLoading ? (
+                    words.length > 0? (
+                        <>
+                            <WordList words={words}/>
+                        </>
+                    ): (
+                        <Typography variant={"h3"}>
+                            No results found for "{searchString}"
+                        </Typography>
+                    )
+                ): <CircularProgress size={69}/>}
+                <div className="flex justify-center">
+                    <Pagination count={numPages}
+                                size={"large"}
+                                color={"primary"}
+                                onChange={(_, page) => setPageNum(page)}/>
+                </div>
+            </CenteredContainer>
         </>
     );
 }
