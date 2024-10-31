f40af82acca6cde33e9d6a40f483e55087afe095
Zachary <zjobby52@gmail.com>
2024-11-01 02:57:47 +0800

    Added loading feedback

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header.tsx";
 import HeroBanner from "../components/HeroBanner.tsx";
 import WordList from "../components/WordList.tsx";
-import {Pagination} from "@mui/material";
+import {CircularProgress, Pagination} from "@mui/material";
 import {useEffect, useState} from "react";
 import {toast} from "sonner";
 import {TOAST_CUSTOM_CLOSE_BTN} from "../common/toast-custom-close-btn.tsx";
@@ -9,16 +9,20 @@ import DictionaryService from "../service/dictionary-service.ts";
 import Word from "../dto/Word.ts";
 
 function Home() {
-    const [numPages, setNumPages] = useState<number>(1);
+    const [numPages, setNumPages] = useState<number>(0);
     const [words, setWords] = useState<Word[]>([]);
     const [pagedWords, setPagedWords] = useState<Word[]>([]);
     const numResultsDisplay: number = 10;
 
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
         const controller = new AbortController();
 
+        setIsLoading(true);
         (async() => {
             try {
+                await new Promise(resolve => setTimeout(resolve, 1000));
                 const response: Word[] = await DictionaryService.getRandomWords(controller);
                 setNumPages(Math.ceil(response.length / numResultsDisplay));
                 setWords(response);
@@ -27,28 +31,33 @@ function Home() {
                 if(error.name !== 'CanceledError')
                     toast.error(error.message, TOAST_CUSTOM_CLOSE_BTN);
             }
+            setIsLoading(false);
         })()
 
         return () => controller.abort();
     }, [])
 
     return (
-    <>
-        <Header enableSearchBar={false}/>
-        <div className={"flex flex-col gap-6 justify-center mx-auto my-12 w-3/5"}>
-            <HeroBanner/>
-            <WordList words={pagedWords} />
-            <div className="flex justify-center">
-                <Pagination count={numPages}
-                            size={"large"}
-                            color={"primary"}
-                            sx={{}}
-                            onChange={(_, page) =>
-                                setPagedWords(words.slice((page - 1) * numResultsDisplay, (page * numResultsDisplay)))}/>
+        <>
+            <Header enableSearchBar={false}/>
+            <div className={"flex flex-col gap-6 justify-center items-center mx-auto my-12 w-3/5"}>
+                <HeroBanner/>
+                {!isLoading ? (
+                    <>
+                        <WordList words={pagedWords}/>
+                        <div className="flex justify-center">
+                            <Pagination count={numPages}
+                                        size={"large"}
+                                        color={"primary"}
+                                        sx={{}}
+                                        onChange={(_, page) =>
+                                            setPagedWords(words.slice((page - 1) * numResultsDisplay, (page * numResultsDisplay)))}/>
+                        </div>
+                    </>
+                    ) : <CircularProgress size={69}/>}
             </div>
-        </div>
-    </>
-)
+        </>
+    );
 }
 
 export default Home
