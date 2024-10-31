533d62ec69a6c62939fc09ccc7d49af10e9d3da0
Zachary <zjobby52@gmail.com>
2024-11-01 04:20:38 +0800

    Use CenteredContainer and minor UI and bug fixes
    (cherry picked from commit 34c6233fe5b21f34c00d0aca696e38f1f0363189)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import {toast} from "sonner";
 import {TOAST_CUSTOM_CLOSE_BTN} from "../common/toast-custom-close-btn.tsx";
 import DictionaryService from "../service/dictionary-service.ts";
 import Word from "../dto/Word.ts";
+import CenteredContainer from "../components/CenteredContainer.tsx";
 
 function Home() {
     const [numPages, setNumPages] = useState<number>(0);
@@ -22,7 +23,6 @@ function Home() {
         setIsLoading(true);
         (async() => {
             try {
-                await new Promise(resolve => setTimeout(resolve, 1000));
                 const response: Word[] = await DictionaryService.getRandomWords(controller);
                 setNumPages(Math.ceil(response.length / numResultsDisplay));
                 setWords(response);
@@ -34,28 +34,30 @@ function Home() {
             setIsLoading(false);
         })()
 
-        return () => controller.abort();
+        return () => {
+            controller.abort();
+            setIsLoading(false);
+        }
     }, [])
 
     return (
         <>
             <Header enableSearchBar={false}/>
-            <div className={"flex flex-col gap-6 justify-center items-center mx-auto my-12 w-3/5"}>
+            <CenteredContainer>
                 <HeroBanner/>
                 {!isLoading ? (
                     <>
                         <WordList words={pagedWords}/>
-                        <div className="flex justify-center">
-                            <Pagination count={numPages}
-                                        size={"large"}
-                                        color={"primary"}
-                                        sx={{}}
-                                        onChange={(_, page) =>
-                                            setPagedWords(words.slice((page - 1) * numResultsDisplay, (page * numResultsDisplay)))}/>
-                        </div>
                     </>
                     ) : <CircularProgress size={69}/>}
-            </div>
+                <div className="flex justify-center">
+                    <Pagination count={numPages}
+                                size={"large"}
+                                color={"primary"}
+                                onChange={(_, page) =>
+                                    setPagedWords(words.slice((page - 1) * numResultsDisplay, (page * numResultsDisplay)))}/>
+                </div>
+            </CenteredContainer>
         </>
     );
 }
