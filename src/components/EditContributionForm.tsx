27ad148badb3ebc80c9de59ab41f8f2d846198ef
Zachary <zjobby52@gmail.com>
2024-11-08 20:27:37 +0800

    Removed unnecessary outer tag and indentation

--- src/components/EditContributionForm.tsx
+++ src/components/EditContributionForm.tsx
@@ -56,120 +56,118 @@ function EditContributionForm() {
     }
 
     return (
-        <>
-            <FormContainer headerString={"Editing"}>
-                <form onSubmit={handleSubmit(handleFormSubmit)}
-                      className={"mt-10 w-1/2"}>
-                    <div className={"mb-1 flex flex-col justify-between gap-6"}>
-                        {/*word or phrase section*/}
-                        <FieldLabel title={"Word or phrase"}/>
-                        <TextField type={"text"}
-                                   placeholder={"e.g. tokou"}
-                                   error={errors.word && true}
-                                   helperText={errors.word?.message}
-                                   className={"w-11/12"}
-                                   {...register('word')}/>
+        <FormContainer headerString={"Editing"}>
+            <form onSubmit={handleSubmit(handleFormSubmit)}
+                  className={"mt-10 w-1/2"}>
+                <div className={"mb-1 flex flex-col justify-between gap-6"}>
+                    {/*word or phrase section*/}
+                    <FieldLabel title={"Word or phrase"}/>
+                    <TextField type={"text"}
+                               placeholder={"e.g. tokou"}
+                               error={errors.word && true}
+                               helperText={errors.word?.message}
+                               className={"w-11/12"}
+                               {...register('word')}/>
 
-                        {/*translation input section*/}
-                        <FieldLabel title={"Translation"}/>
-                        {translationFields.map((field, index) => {
-                            return (
-                                <div key={field.id} className={"flex flex-col gap-6"}>
-                                    <div className={"flex gap-3 items-center"}>
-                                        <TextField placeholder={`translation ${index + 1}`}
-                                                   rows={3}
-                                                   maxRows={3}
-                                                   error={errors.translations?.[index]?.translation && true}
-                                                   helperText={errors.translations?.[index]?.translation?.message}
-                                                   className={"w-11/12"}
-                                                   {...register(`translations.${index}.translation`)}/>
+                    {/*translation input section*/}
+                    <FieldLabel title={"Translation"}/>
+                    {translationFields.map((field, index) => {
+                        return (
+                            <div key={field.id} className={"flex flex-col gap-6"}>
+                                <div className={"flex gap-3 items-center"}>
+                                    <TextField placeholder={`translation ${index + 1}`}
+                                               rows={3}
+                                               maxRows={3}
+                                               error={errors.translations?.[index]?.translation && true}
+                                               helperText={errors.translations?.[index]?.translation?.message}
+                                               className={"w-11/12"}
+                                               {...register(`translations.${index}.translation`)}/>
 
-                                        {translationFields.length > 1 &&
-                                            <IconButton color={"error"}
-                                                        onClick={() => removeTranslation(index)}>
-                                                <RemoveCircle fontSize={"large"}
-                                                              color={"error"}/>
-                                            </IconButton>
-                                        }
-                                    </div>
+                                    {translationFields.length > 1 &&
+                                        <IconButton color={"error"}
+                                                    onClick={() => removeTranslation(index)}>
+                                            <RemoveCircle fontSize={"large"}
+                                                          color={"error"}/>
+                                        </IconButton>
+                                    }
                                 </div>
-                            );
-                        })}
-                        <Button variant={"contained"}
-                                className={"w-1/3 capitalize bg-gray-900"}
-                                onClick={() => appendTranslation({
-                                    translationId: null,
-                                    translation: ''
-                                })}>
-                            Add translation <AddCircle fontSize={"small"}
-                                                       className={"ml-2"}/>
-                        </Button>
+                            </div>
+                        );
+                    })}
+                    <Button variant={"contained"}
+                            className={"w-1/3 capitalize bg-gray-900"}
+                            onClick={() => appendTranslation({
+                                translationId: null,
+                                translation: ''
+                            })}>
+                        Add translation <AddCircle fontSize={"small"}
+                                                   className={"ml-2"}/>
+                    </Button>
 
-                        {/*examples section*/}
-                        <FieldLabel title={"Usage examples"}/>
-                        {exampleFields.map((field, index) => {
-                            return(
-                                <div key={field.id} className={"flex flex-col gap-6"}>
-                                    <div className={"flex flex-col gap-3"}>
-                                        <div className={"flex gap-3 items-center"}>
-                                            <TextField multiline
-                                                       rows={3}
-                                                       placeholder={`example ${index + 1}`}
-                                                       error={errors.usageExamples?.[index]?.example && true}
-                                                       helperText={errors.usageExamples?.[index]?.example?.message}
-                                                       className={"w-11/12"}
-                                                       {...register(`usageExamples.${index}.example` as const)}/>
-                                            {exampleFields.length > 1 &&
-                                                <IconButton color={"error"}
-                                                            onClick={() => removeExample(index)}>
-                                                    <RemoveCircle fontSize={"large"}
-                                                                  color={"error"}/>
-                                                </IconButton>}
-                                        </div>
-
+                    {/*examples section*/}
+                    <FieldLabel title={"Usage examples"}/>
+                    {exampleFields.map((field, index) => {
+                        return(
+                            <div key={field.id} className={"flex flex-col gap-6"}>
+                                <div className={"flex flex-col gap-3"}>
+                                    <div className={"flex gap-3 items-center"}>
                                         <TextField multiline
                                                    rows={3}
-                                                   placeholder={`Translate your example`}
-                                                   error={errors.usageExamples?.[index]?.exampleTranslation && true}
-                                                   helperText={errors.usageExamples?.[index]?.exampleTranslation?.message}
+                                                   placeholder={`example ${index + 1}`}
+                                                   error={errors.usageExamples?.[index]?.example && true}
+                                                   helperText={errors.usageExamples?.[index]?.example?.message}
                                                    className={"w-11/12"}
-                                                   {...register(`usageExamples.${index}.exampleTranslation` as const)}/>
+                                                   {...register(`usageExamples.${index}.example` as const)}/>
+                                        {exampleFields.length > 1 &&
+                                            <IconButton color={"error"}
+                                                        onClick={() => removeExample(index)}>
+                                                <RemoveCircle fontSize={"large"}
+                                                              color={"error"}/>
+                                            </IconButton>}
                                     </div>
-                                </div>
-                            );
-                        })}
-                        <Button variant={"contained"}
-                                className={"w-1/3 capitalize bg-gray-900"}
-                                onClick={() => appendExample({
-                                    exampleId: null,
-                                    example: '',
-                                    exampleTranslation: ''
-                                })}>
-                            Add example <AddCircle fontSize={"small"}
-                                                   className={"ml-2"}/>
-                        </Button>
-                    </div>
 
-                    {/*submit and reset buttons*/}
-                    <div className={"flex w-11/12 gap-3"}>
-                        <Button variant={"contained"}
-                                type={"submit"}
-                                disabled={isSubmitting}
-                                className={"mt-6 capitalize w-1/2 text-lg"}>
-                            {isSubmitting ? <CircularProgress size={25}/> : "Edit"}
-                        </Button>
-                        <Button variant={"contained"}
-                                type={"reset"}
-                                disabled={isSubmitting}
-                                onClick={() => navigate(`/definition/${wordId}`)}
-                                className={"mt-6 capitalize w-1/2 text-lg"}
-                                sx={{backgroundColor: "#f28b82"}}>
-                            Cancel
-                        </Button>
-                    </div>
-                </form>
-            </FormContainer>
-        </>
+                                    <TextField multiline
+                                               rows={3}
+                                               placeholder={`Translate your example`}
+                                               error={errors.usageExamples?.[index]?.exampleTranslation && true}
+                                               helperText={errors.usageExamples?.[index]?.exampleTranslation?.message}
+                                               className={"w-11/12"}
+                                               {...register(`usageExamples.${index}.exampleTranslation` as const)}/>
+                                </div>
+                            </div>
+                        );
+                    })}
+                    <Button variant={"contained"}
+                            className={"w-1/3 capitalize bg-gray-900"}
+                            onClick={() => appendExample({
+                                exampleId: null,
+                                example: '',
+                                exampleTranslation: ''
+                            })}>
+                        Add example <AddCircle fontSize={"small"}
+                                               className={"ml-2"}/>
+                    </Button>
+                </div>
+
+                {/*submit and reset buttons*/}
+                <div className={"flex w-11/12 gap-3"}>
+                    <Button variant={"contained"}
+                            type={"submit"}
+                            disabled={isSubmitting}
+                            className={"mt-6 capitalize w-1/2 text-lg"}>
+                        {isSubmitting ? <CircularProgress size={25}/> : "Edit"}
+                    </Button>
+                    <Button variant={"contained"}
+                            type={"reset"}
+                            disabled={isSubmitting}
+                            onClick={() => navigate(`/definition/${wordId}`)}
+                            className={"mt-6 capitalize w-1/2 text-lg"}
+                            sx={{backgroundColor: "#f28b82"}}>
+                        Cancel
+                    </Button>
+                </div>
+            </form>
+        </FormContainer>
     );
 }
 
