9552794adc96990e332c2297d7f72d5bbc7dd260
Zachary <zjobby52@gmail.com>
2024-11-09 07:35:32 +0800

    added breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ export default {
           colors: {
               primary: "#f5bd02"
           },
+          screens: {
+              'xs': '480px',
+              'xxs': '320px',
+          },
       },
   },
   corePlugins: {
