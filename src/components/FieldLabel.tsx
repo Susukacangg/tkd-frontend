6b8327e5dc12652f174bf1bd954e10d3e2a6d0b7
Zachary <zjobby52@gmail.com>
2024-11-08 21:37:54 +0800

    Adjust common form components and word details form for mobile viewing

--- src/components/FieldLabel.tsx
+++ src/components/FieldLabel.tsx
@@ -2,7 +2,7 @@ import {Typography} from "@mui/material";
 
 function FieldLabel({title}: {title: string}) {
     return(
-        <Typography variant={"h6"} className={"-mb-3"}>
+        <Typography variant={"h6"} className={"-mb-3 sm:text-5xl xl:text-xl"}>
             {title}
         </Typography>
     );
--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -3,11 +3,12 @@ import FormContainerProps from "../component-props/form-container-props.ts";
 
 function FormContainer({headerString, subHeaderString, children, formFooter}: FormContainerProps) {
     return (
-        <div className={"container flex flex-col justify-center items-start w-full mt-10 mb-14 mx-auto"}>
-            <Typography variant={"h4"} fontWeight={"bold"}>
+        <div className={"container flex flex-col justify-center items-start w-full sm:mt-24 xl:mt-10 mb-14 mx-auto"}>
+            <Typography variant={"h4"} fontWeight={"bold"}
+                        className={"sm:text-6xl xl:text-4xl"}>
                 {headerString}
             </Typography>
-            {subHeaderString && <Typography className={"mt-3"} variant={"body1"}>
+            {subHeaderString && <Typography className={"sm:mt-8 xl:mt-3 sm:text-2xl xl:text-md"} variant={"body1"}>
                 {subHeaderString}
             </Typography>}
 
