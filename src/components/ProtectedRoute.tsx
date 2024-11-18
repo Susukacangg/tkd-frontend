21f1992e206ff08cd9113f0d7b26598224313cfd
Zachary <zjobby52@gmail.com>
2024-11-19 00:32:02 +0800

    refactored code for modularity

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
 import {useAuth} from "../contexts/AuthContext.tsx";
 import {Navigate} from "react-router-dom";
 import {JSX} from "react";
+import LoadingPage from "../pages/LoadingPage.tsx";
 
 function ProtectedRoute({children}: {children: JSX.Element}) {
-    const {isAuthenticated} = useAuth();
+    const {isAuthenticated, isAuthenticating} = useAuth();
 
-    if(isAuthenticated)
-        return children;
-    else
-        return <Navigate to={"/login"}/>
+    if (isAuthenticating)
+        return <LoadingPage loadingText={"Verifying user"}/>
+
+    return isAuthenticated ? children : <Navigate to={"/login"}/>;
 }
 
 export default ProtectedRoute;
